1a57d3a1a40d5dfb96e9c9d1f418d1776e844742
Hiroko ABE <hihoyeahi@gmail.com>
2020-03-17 22:20:48 +0900

    topの調整

--- js/daiary.js
+++ js/daiary.js
@@ -19,19 +19,22 @@ $('.daiary_cat-10').css('background', 'green');
 $('.daiary_cat-11').css('background', 'red');
 
 
-var property = window.getComputedStyle($('.card_title')[0], '::before').getPropertyValue('background-color');
-var sheets = document.styleSheets,
-  sheet = sheets[sheets.length - 1];
-  console.log(sheets);
-  console.log(property);
+var property = window.getComputedStyle($('.card-title')[0], '::before').getPropertyValue('background-color');
+  var sheets = document.styleSheets,
+    sheet = sheets[10];
+    console.log(property);
+    console.log(sheets);
+    console.log(sheet);
 
-$(function() {
 
-  if (".card-body").children('div').hassClass('daiary_cat-9') {	// Webkit
-    sheet.insertRule('.card_title::before' , 'background-color:' + property + '"yellow" }', sheet.cssRules.length);
-  }
-  else if (sheet.addRule) {  // IE
-    sheet.addRule('.card_title::before', 'background-color:' + property + '"yellow"', -1);
-  }
-});
+  $(function() {
 
+    if($(".card-title").parent().hasClass('daiary_cat-9')) {   // Webkit
+      sheet.insertRule('.diary-content-1 .card-title::before{ background:yellow }', sheet.cssRules.length);
+    }
+    else if (sheet.addRule) {  // IE
+      sheet.addRule('.diary-content-1 .card-title::before{ background:yellow }', -1);
+    }
+
+    console.log(sheet.cssRules.length);
+  });
